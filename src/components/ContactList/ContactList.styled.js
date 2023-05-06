505b7f88e0ae6135c067f0d4770df2cdfb20a06d
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-06 20:29:26 +0300

    Create ContactList.styled.js

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  list-style: square;
+`;
+
+export const ListItem = styled.li`
+display:flex;
+margin-top: 15px;
+align-items: center;
+`
+
+export const Button = styled.button`
+  margin-left: 15px;
+  border-radius: 5px;
+  padding: 5px;
+  box-shadow: 0px 0px 2px 2px rgb(0, 0, 0);
+  cursor: pointer;
+`;
